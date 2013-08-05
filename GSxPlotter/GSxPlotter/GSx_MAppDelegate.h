ece2231843cc2cf9401ebe0031b014f37aad4620
Aaron <a_augsburger@mac.com>
2013-08-04 20:07:24 -0400

    Had to do another push in Xcode to get this stuff working properly after changing to a new Provisioning Profile.

---
+++ GSxPlotter/GSxPlotter/GSx_MAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GSx_MAppDelegate.h
+//  GSxPlotter
+//
+//  Created by Aaron Augsburger on 7/30/13.
+//  Copyright (c) 2013 Aaron Augsburger. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GSx_MAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ GSxPlotter/GSxPlotter/GSx_MAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  GSx_MAppDelegate.m
+//  GSxPlotter
+//
+//  Created by Aaron Augsburger on 7/30/13.
+//  Copyright (c) 2013 Aaron Augsburger. All rights reserved.
+//
+
+#import "GSx_MAppDelegate.h"
+
+@implementation GSx_MAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
