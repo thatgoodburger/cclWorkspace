35d9edb0c8d997c8bb7240459a0cb9609a9a28a0
Aaron <a_augsburger@mac.com>
2013-09-27 10:18:51 -0400

    Updated Arduino Code
    - Arduino now uses strings for command input.

--- Arduino Controller/pingHello/pingHello.ino
+++ Arduino Controller/pingHello/pingHello.ino
@@ -1,20 +1,54 @@
+String inputString = "";           // string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+int inByte = 0;                  // incoming serial byte
+
+
 void setup() 
 {
+  // initialize serial:
   Serial.begin(9600);
+  // reserve some bytes for the inputString:
+  inputString.reserve(200);
+  // broadcast a message:
+  establishContact();
 }
 
 void loop() 
 {
-  Serial.write("h");   delay(250);
-  Serial.write("e");   delay(250);
-  Serial.write("l");   delay(250);
-  Serial.write("l");   delay(250);
-  Serial.write("o");   delay(250);
-  
-  for(int i=0; i<5; ++i)
-  {
-    Serial.write("."); delay(250); 
+  // print the string when a newline arrives
+  if (stringComplete) {
+    
+    if(!strcmp(inputString.c_str(), "Hello")){
+      Serial.println("Don't look at me in the eyes."); 
+    }
+    
+    Serial.println(inputString);
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
   }
-  
-  Serial.write("?\n");  delay(500);
+}
+
+// runs between loop runs, detects data via serial RX
+void serialEvent() 
+{
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // detect a newline to tell main about completion:
+    if (inChar == '\n')
+      stringComplete = true;
+    else
+      // add to the inputString:
+      inputString += inChar;
+  }
+}
+
+// initial means of contacting PC
+void establishContact()
+{
+  while (Serial.available() <= 0) {
+    Serial.println("0,0,0");
+    delay(300);
+  }
 }
